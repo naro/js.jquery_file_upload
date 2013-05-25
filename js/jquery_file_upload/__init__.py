d72953ef100b86ea9117f76cc888fb292361c307
Radim Novotny <novotny.radim@gmail.com>
2013-05-25 14:56:31 +0200

    Updated to jQuery File Upload 8.2.1. Minified versions are missing

--- js/jquery_file_upload/__init__.py
+++ js/jquery_file_upload/__init__.py
@@ -16,28 +16,66 @@ library = Library(
     )
 
 # JS
+# Libs
+canvas_to_blob = Resource(
+    library,
+    'js/libs/canvas-to-blob.min.js',
+    )
+load_image = Resource(
+    library,
+    'js/libs/load-image.min.js',
+    )
+tmpl = Resource(
+    library,
+    'js/libs/tmpl.min.js',
+    )
+# file upload JS
 iframe_transport = Resource(
     library,
     'js/jquery.iframe-transport.js',
-    minified='js/jquery.iframe-transport.min.js',
-    depends=[jquery, ]
+    # minified='js/jquery.iframe-transport.min.js',
+    depends=[jquery]
     )
 fileupload = Resource(
     library,
     'js/jquery.fileupload.js',
-    minified='js/jquery.fileupload.min.js',
-    depends=[iframe_transport, ui_widget, ]
+    # minified='js/jquery.fileupload.min.js',
+    depends=[jquery, iframe_transport, ui_widget, ]
+    )
+fileupload_process = Resource(
+    library,
+    'js/jquery.fileupload-process.js',
+    # minified='js/jquery.fileupload-process.min.js',
+    depends=[jquery, fileupload]
+    )
+fileupload_resize = Resource(
+    library,
+    'js/jquery.fileupload-resize.js',
+    # minified='js/jquery.fileupload-resize.min.js',
+    depends=[load_image, canvas_to_blob, fileupload_process]
+    )
+fileupload_validate = Resource(
+    library,
+    'js/jquery.fileupload-validate.js',
+    # minified='js/jquery.fileupload-validate.min.js',
+    depends=[jquery, fileupload_process]
+    )
+fileupload_ui = Resource(
+    library,
+    'js/jquery.fileupload-ui.js',
+    # minified='js/jquery.fileupload-ui.min.js',
+    depends=[jquery, tmpl, fileupload_resize, fileupload_validate]
     )
 postmessage_transport = Resource(
     library,
     'js/cors/jquery.postmessage-transport.js',
-    minified='js/cors/jquery.postmessage-transport.min.js',
+    # minified='js/cors/jquery.postmessage-transport.min.js',
     depends=[jquery, ]
     )
 xdr_transport = Resource(
     library,
     'js/cors/jquery.xdr-transport.js',
-    minified='js/cors/jquery.xdr-transport.min.js',
+    # minified='js/cors/jquery.xdr-transport.min.js',
     depends=[jquery, ]
     )
 
@@ -45,10 +83,10 @@ xdr_transport = Resource(
 ui_noscript = Resource(
     library,
     'css/jquery.fileupload-ui-noscript.css',
-    minified='css/jquery.fileupload-ui-noscript.min.css',
+    # minified='css/jquery.fileupload-ui-noscript.min.css',
     )
 ui = Resource(
     library,
     'css/jquery.fileupload-ui.css',
-    minified='css/jquery.fileupload-ui.min.css',
+    # minified='css/jquery.fileupload-ui.min.css',
     )
--- js/jquery_file_upload/resources/README.txt
+++ js/jquery_file_upload/resources/README.txt
@@ -1,1 +1,3 @@
 Copy the library resources here and register them in __init__.py.
+Current jQuery File Upload version: 8.2.1
+https://github.com/blueimp/jQuery-File-Upload/tags
